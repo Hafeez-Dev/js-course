9d470bfec538a6465615fa5e0509b8675995ba4e
Abdul Hafeez <debugerxy@gmail.com>
2024-07-22 12:39:57 +0500

    dataTypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3) //we are using nodejs, not browser
+console.log(3 
+    + 3);   //code readability should high
+
+console.log('Hafeez');
+
+let name = "Abdul Hafeez"
+let age = 18
+let isLogged = false
+
+// number => 2 to power 53     |
+// bigint                      |
+// string => ""                |
+// bollean => true/false        >>    Primitive data types
+// null => standalone value    |
+// undefined =>                | 
+// symbol => uniqe             |
+
+//objects                      |
+//                              >>      Non premitive data types
+
+console.log(typeof undefined);
+console.log(typeof null);
