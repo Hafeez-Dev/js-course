8380d286e4147afc01b386c9038e9779e95097ec
Abdul Hafeez <debugerxy@gmail.com>
2024-07-23 11:38:02 -0700

    conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,22 @@
+let score = false;
+
+console.log(score);
+console.log(typeof score);
+
+let valueInNumber = Number(score) //convert to Number
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "3abc" => NaN
+// true => 1; false => 0
+
+let loggedIn = 1;
+let booleanLogged = Boolean(loggedIn)
+console.log( booleanLogged);
+
+let someNum = 33;
+let stringNum = String(someNum)
+console.log(stringNum);
+console.log(typeof stringNum);
