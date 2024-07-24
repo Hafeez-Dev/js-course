4cd65a88396be6986ea604632910eec2dd881a77
Abdul Hafeez <debugerxy@gmail.com>
2024-07-24 09:53:22 +0500

    Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,22 +1,62 @@
-let score = false;
+// let score = false;
 
-console.log(score);
-console.log(typeof score);
+// console.log(score);
+// console.log(typeof score);
 
-let valueInNumber = Number(score) //convert to Number
+// let valueInNumber = Number(score) //convert to Number
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "3abc" => NaN
 // true => 1; false => 0
 
-let loggedIn = 1;
-let booleanLogged = Boolean(loggedIn)
-console.log( booleanLogged);
+// let loggedIn = 1;
+// let booleanLogged = Boolean(loggedIn)
+// console.log( booleanLogged);
 
-let someNum = 33;
-let stringNum = String(someNum)
-console.log(stringNum);
-console.log(typeof stringNum);
+// let someNum = 33;
+// let stringNum = String(someNum)
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+// ********************* Operations *********************
+let value = 5
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/8);
+// console.log(2**4);
+// console.log(2*3);
+// console.log(2%2);
+
+let str1 = "Abdul"
+let str2 = " Hafeez"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(+true);
+console.log(+"");
+
+let num1 = num2 = num3 = 2 + 2;
+console.log(num3);
+
+let gameScore = 100
+let test = gameScore++
+//gameScore = 101, test = 100
+
+console.log(test);
+
+let gameScore2 = 100
+let test2 = ++gameScore2
+//gameScore = 101, test = 101
+
+console.log(test2);
