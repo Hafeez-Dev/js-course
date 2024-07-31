1895a2d8649dfbf1502851fd29bf1a49ab5d449e
Abdul Hafeez <debugerxy@gmail.com>
2024-07-31 08:25:03 +0500

    mini scope and hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,3 +1,5 @@
+//Scopes in JS
+
 var c = 300
 let a = 300
 if(true) {
@@ -11,3 +13,44 @@ if(true) {
 console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one() {
+    const username = "Hafeez"
+
+    function two() {
+        const website = " YouTube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+
+// one()
+
+
+if (true) {
+    const username = "Abdul Hafeez"
+    if (username === "Abdul Hafeez") {
+        const website = " YouTube"
+        console.log(username + website);
+    }
+
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// +++++++++++++++++++++++++++++++ Hoisting ++++++++++++++++++++++++++++++
+
+
+console.log(addone(5)) //returned 6
+function addone(num) {
+    return num + 1
+}
+
+addtwo(8) //Cannot access 'addtwo' before initialization
+const addtwo = function(num) {
+    return num + 2
+}
