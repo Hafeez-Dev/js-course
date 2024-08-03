cd1b425ebd240784c4c5e2ac3d0a964aece72bd9
Abdul Hafeez <debugerxy@gmail.com>
2024-08-03 10:29:18 +0500

    while and do-while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+let index = 0;
+
+while (index <= 10) {
+    // console.log(index);
+    index = index + 2
+}
+
+let myArray = ["flash", "batman", "superman"]
+let arr = 0;
+while (arr < myArray.length) {
+    // console.log(myArray[arr]);
+    arr = arr + 1;
+}
+
+let score = 14
+
+do {
+    console.log(`Score is ${score}`);
+    score = score + 1
+} while (score <= 10);
